2fa0c080438ec57b50d9632ea271888138f0ad04
Michael Murphy <Michael.Murphy@enable.com>
2022-07-08 14:07:35 +0100

    Initial commit - some Git helper functions

--- GitCmdlet.psm1
+++ GitCmdlet.psm1
@@ -1,17 +1,26 @@
 function Get-Branch {
+  # Gets the current branch
   return &git branch | ForEach-Object -Process { if ($_.StartsWith("*")) { $_.Substring(2) } }
 }
 
 function New-Branch {
   param (
-    [string] $Name
+    [Parameter(Mandatory=$true)]
+    [string] $BranchName,
+    [string] $Initials = "mu",
+    [switch] $Bug = $false,
+    [switch] $Devops = $false
   )
 
-  if (!$name.StartsWith("feature") -and !$Name.StartsWith("bug")) {
-    $Name = "feature/mu/$Name"
+  if ($Bug) {
+    $BranchName = "bug/$Initials/$BranchName"
+  } elseif ($Devops) {
+    $BranchName = "devops/$Initials/$BranchName"
+  } else {
+    $BranchName = "feature/$Initials/$BranchName"
   }
 
-  $result = &git checkout -b $Name
+  $result = &git checkout -b $BranchName
   Write-Host $result -ForegroundColor green
 }
 
@@ -27,6 +36,14 @@ function Push-Branch {
   return $result
 }
 
+function Remove-Branches {
+  # Delete all branches except main, master, or current
+  &git branch | Select-String -Pattern '^(?!.*(\*|main|master)).*$' | ForEach-Object { git branch -D $_.ToString().Trim() }
+  Write-Host "Remaining branches:"
+  &git branch
+}
+
 Export-ModuleMember -Function "Get-Branch"
 Export-ModuleMember -Function "New-Branch"
 Export-ModuleMember -Function "Push-Branch"
+Export-ModuleMember -Function "Remove-Branches"
